393d0533b2d01ce2dc87c75d61c7aeca121be701
OleksandrBodnarchuk <obodnarchuk@outlook.com>
2021-11-23 18:56:59 +0100

    Login, logout forms and logic

---
+++ src/main/java/pl/alex/devnote/authentication/LoginController.java
@@ -0,0 +1,21 @@
+package pl.alex.devnote.authentication;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+@ServletSecurity(
+        httpMethodConstraints ={
+                @HttpMethodConstraint(value = "GET",rolesAllowed = "USER")
+        }
+)
+public class LoginController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.sendRedirect(request.getContextPath());
+    }
+
+}
